4fba2058821639bd6b783caeaf19a4cab45e469a
mikmikmedia <mikmikmedia@gmail.com>
2018-12-03 12:40:08 +0100

    initial commit

---
+++ Transport/Controllers/SupportStatuseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Transport.Controllers
+{
+    public class SupportStatuseController : Controller
+    {
+        // GET: SupportStatuses
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Transport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Transport.Startup))]
+namespace Transport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
